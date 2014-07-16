0dd694f6dad2d0f7dd2720f5e0ccfea2fd4ee2e5
test1 <test1@123.com>
2014-07-16 16:00:02 +0800

    file track

--- Browser.h
+++ Browser.h
@@ -25,6 +25,9 @@ void ttt()
 	int x = 1;
 }
 
+/*
 Git is a distributed version control system.
 Git is free software distributed under the GPL.
 Git has a mutable index called stage.
+Git tracks changes.
+*/
